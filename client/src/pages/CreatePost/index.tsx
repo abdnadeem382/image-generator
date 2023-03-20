377ce28a1fe428c0de9433efb58404c644a60219
Abdullah Nadeem <abdnadeem382@gmail.com>
2023-03-20 18:23:29 +0500

    create post basic ui

--- client/src/pages/CreatePost/index.tsx
+++ client/src/pages/CreatePost/index.tsx
@@ -6,5 +6,54 @@ import { getRandomPrompt } from "../../utils";
 import { Loader, FormField } from "../../components";
 
 export default function CreatePost() {
-  return <div>CreatePost</div>;
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {};
+
+  const handleChange = () => {};
+
+  const handleSurpriseMe = () => {};
+
+  return (
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[32px] text-darkText">Create</h1>
+        <p className="mt-2 text-lightText max-w-[500px]">
+          Create imaginative and visually stunning images through DALL-E AI and
+          share them with the community
+        </p>
+      </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="John Doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="an oil painting by Matisse of a humanoid robot playing chess"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  );
 }
