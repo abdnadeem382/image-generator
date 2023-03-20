e6fe090a7a72cee57f0636ff1e0a854c66d7adf7
Abdullah Nadeem <abdnadeem382@gmail.com>
2023-03-20 18:24:03 +0500

    removed unused imports

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import { If, Else, Then, When } from "react-if";
 
 import { Loader, Card, FormField } from "../../components";
