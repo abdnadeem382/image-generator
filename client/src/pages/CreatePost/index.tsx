2340b4394516a5f959c8723955643f755b33dfca
Abdullah Nadeem <abdnadeem382@gmail.com>
2023-03-20 18:10:54 +0500

    created random prompt func

--- client/src/pages/CreatePost/index.tsx
+++ client/src/pages/CreatePost/index.tsx
@@ -1,4 +1,9 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { preview } from "../../assets";
+import { getRandomPrompt } from "../../utils";
+import { Loader, FormField } from "../../components";
 
 export default function CreatePost() {
   return <div>CreatePost</div>;
